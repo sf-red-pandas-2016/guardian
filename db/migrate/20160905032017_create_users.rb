9f5d886354a5003596bf5de43ed1b16605026b90
shawn <shawn@naturalprofessional.com>
2016-09-04 20:54:08 -0700

    Add Devise

--- db/migrate/20160905032017_create_users.rb
+++ db/migrate/20160905032017_create_users.rb
@@ -3,12 +3,11 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
       t.string :password
       t.string :phone_number
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
+    
   end
 end
