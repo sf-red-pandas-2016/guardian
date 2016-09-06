093e91d34779113dd5282d413aec08162825beb1
shawn <shawn@naturalprofessional.com>
2016-09-06 15:47:23 -0700

    Create ajax call to create new event on Walk Me Home button click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,21 @@ $(document).ready(function(){
          url: "/sms/text_friend"
        })
        .done(function(serverResponse){
-         console.log(serverResponse + " This is the post-send-text server response");
+         console.log(serverResponse + " -- This is the post-send-text server response");
        })
        .fail(function(serverResponse){
          console.log("Request failed");
        })
+     $.ajax({
+          url: "/events",
+          method: "POST"
+        })
+        .done(function(serverResponse){
+          console.log(serverResponse + " -- This is the event-created server response");
+        })
+        .fail(function(serverResponse){
+          console.log("New event creation failed");
+        })
     $.ajax({
       url: "http://172.16.50.232:8080/start",
       crossDomain : true,
@@ -21,28 +31,27 @@ $(document).ready(function(){
     .done(function(serverResponse){
       $("#walk-me-home").hide();
       $("#home-safely").show();
-      console.log(serverResponse + "This is the start-drone server response");
+      console.log(serverResponse + " -- This is the start-drone server response");
     })
   // need to add failure response
   })
 
-  $("#home-safely").on("click", function(e){
-    console.log("I'm home!!")
+  $("#home-safely img").on("click", function(e){
     e.preventDefault();
     $.ajax({
       url: "http://172.16.50.232:8080/end",
       crossDomain : true,
     })
     .done(function(serverResponse){
-      $("#home-safely").hide();
-      $("#show-events").show();
-      console.log(serverResponse + "This is the stop-drone server response");
+      console.log(serverResponse + " -- This is the stop-drone server response");
     })
     $.ajax({
       url: "/sms/text_friend_once_home"
     })
     .done(function(serverResponse){
-      console.log(serverResponse + " This is the text-once-home server response");
+      console.log(serverResponse + " -- This is the text-once-home server response");
+      $(this).hide();
+      $("#show-events").show();
     })
     .fail(function(serverResponse){
       console.log("Request failed");
