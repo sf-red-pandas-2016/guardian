cf09ba335a7245962e448c073c840565fe2ac845
shawn <shawn@naturalprofessional.com>
2016-09-06 16:51:33 -0700

    Update text message to include name. Abstractize phone numbers in sms controller

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -2,7 +2,7 @@ class SmsController < ApplicationController
 
 	def text_friend
 		@user = current_user
-		message = "Thanks for watching that I get home safely. Link to watch: http://localhost:3000/events/#"
+		message = "This is " + current_user.first_name + " " + current_user.last_name + "Thanks for watching that I get home safely! Link to watch: http://localhost:3000/events/#"
 
 	 	to = @user.friends.first
 	 	client = Twilio::REST::Client.new(
@@ -11,7 +11,7 @@ class SmsController < ApplicationController
 	  	)
 	  	if client.messages.create(
 	    		to: to.phone_number,
-	    		from: '+16502156875',
+	    		from: current_user.phone_number,
 	     		body: message
 	    	)
 	    	flash[:notice] = "Message has been sent!"
@@ -31,7 +31,7 @@ class SmsController < ApplicationController
 	  	)
 	  	if client.messages.create(
 	    		to: to.phone_number,
-	    		from: '+16502156875',
+	    		from: current_user.phone_number,
 	     		body: message
 	    	)
 	    	flash[:notice] = "Text message to your friend has been sent!"
