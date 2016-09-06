6710273dde4368bff6d9fa0d3c3c8a8f32f62ee4
shawn <shawn@naturalprofessional.com>
2016-09-05 20:53:04 -0700

    Fix logout route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
-  
+  root "home#index"
+
   resources :events
+
+
 end
