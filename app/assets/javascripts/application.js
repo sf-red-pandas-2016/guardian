0e4225903ddf9690e3c7a776bc79808d1e898a9a
shawn <shawn@naturalprofessional.com>
2016-09-05 20:13:19 -0700

    Ajax request that pings another server--woot woot--cors baby

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,17 +3,32 @@ $(document).ready(function(){
   // var turbolinks = require('turbolinks')
 
   $("#walk-me-home").on("click", function(e){
-    console.log("In walk-me-home ")
+    console.log("In walk-me-home")
     e.preventDefault();
 
+    // var wnd = window.open("http://172.16.51.60:3000/start", "_blank" );
+    // wnd.close()
+
     $.ajax({
-      url: "/events#create",
-      method: "POST"
-    })
-    .done(function(serverResponse){
-      console.log(serverResponse + "This is the server response");
+      url: "http://172.16.50.232:8080/start",
+      crossDomain : true,
     })
 
+    .done(function(serverResponse){
+      
+        console.log(serverResponse + "This is the server response");
+    })
+  //
+  // var xhr = new XMLHttpRequest();
+  // xhr.open("get", "http://172.16.50.232:8080/test", true);
+  // xhr.onload = function(){  //instead of onreadystatechange
+  //   //do something
+  //   console.log("Did we get anything back??")
+  // };
+  // xhr.send(null);
   })
+
+
+
 
 });
