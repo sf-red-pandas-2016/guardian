8e5135661cbfe4338af3f354d4561dcf81330163
shawn <shawn@naturalprofessional.com>
2016-09-07 13:11:30 -0700

    Redirect after sign in-- better than last way

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
+  def after_sign_in_path_for(user)
+      user_url(user)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
 
   def index
-    redirect_to '/users/show'
   end
 end
