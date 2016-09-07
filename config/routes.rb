b38c50f8f308649d93ba2768491fe601f12ff45b
shawn <shawn@naturalprofessional.com>
2016-09-07 15:54:30 -0700

    Add Friends controller, resource and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   resources :users
+  resources :friends
+  resources :events
 
   devise_scope :user do
    get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
-
-  resources :events
 
   get '/sms/text_friend' => 'sms#text_friend'
   get '/sms/text_friend_once_home' => 'sms#text_friend_once_home'
