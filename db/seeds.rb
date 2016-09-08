fcf27b95addc93b3a701a2325b93518242f27bf3
codejlo <jsl728@Jasons-MacBook-Air.local>
2016-09-07 19:44:23 -0700

    Style friends and edit even styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,10 @@ Friend.delete_all
 
 User.create(first_name: "Bill", last_name: "Miller", email:"e@c.com", password: "123456",phone_number: "+16502156875")
 
-4.times do 
-  Friend.create(first_name: "Jen", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
-  Friend.create(first_name: "Jake", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
-  Friend.create(first_name: "Juanita", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
-  Friend.create(first_name: "Javier", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
-end
+Friend.create(first_name: "Jen", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
+Friend.create(first_name: "Jake", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
+Friend.create(first_name: "Juanita", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
+Friend.create(first_name: "Javier", last_name: "Miller", user_id: 1, email: "e@b.com", phone_number: "+15304703493")
 
 Drone.create(name: "Patricia", server_address: "http://172.16.50.232:8080/start", user_id: 1)
 
