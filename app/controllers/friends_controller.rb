c5c0add557a58bbcfd345784686a5a19d45f2b34
shawn <shawn@naturalprofessional.com>
2016-09-07 18:14:16 -0700

    Add new friend form and create method in controller

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,10 +5,21 @@ class FriendsController < ApplicationController
   end
 
   def create
+    @friend = Friend.new(friend_params)
+    @friend.user_id = current_user.id
+    if @friend.save
+      redirect_to @friend
+    else
+      render 'new'
+    end
   end
 
-  # def new
-  # end
+  def new
+  end
+
+  def edit
+    @friend = Friend.find(params[:id])
+  end
 
   def destroy
     @friend = Friend.find(params[:id])
@@ -22,7 +33,17 @@ class FriendsController < ApplicationController
   end
 
   def update
+    @friend = Friend.find(params[:id])
+
+    if @friend.update(friend_params)
+      redirect_to @friend
+    else
+      render 'edit'
+    end
   end
 
-
+  private
+  def friend_params
+    params.require(:friend).permit(:first_name, :last_name, :email, :phone_number)
+  end
 end
