897241bbbf006f3c88bf9e842edab4722eec0e67
armoney <armonarcuri@yahoo.com>
2016-09-08 10:05:06 -0700

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,19 @@ Rails.application.routes.draw do
   resources :friends
   resources :events
 
+  resources :user do
+    :events
+  end
+
   devise_scope :user do
    get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :events do
+    get '/stream' => "events#show/stream"
+  end
+
 
   get '/sms/text_friend' => 'sms#text_friend'
   get '/sms/text_friend_once_home' => 'sms#text_friend_once_home'
